ed159446e68dfb672cafb5262eadc2779a5c758c
ThriftyOldStudent <34296983+Asvoria@users.noreply.github.com>
2021-02-22 23:20:49 +0800

    Update 9_Slides_Credits.js

--- js/slides/9_Slides_Credits.js
+++ js/slides/9_Slides_Credits.js
@@ -9,5 +9,11 @@ SLIDES.push({
 			x:0, y:35, width:960, height:400, align:"center",
 			text_id:"credits", color:"#fff"
 		});
+		
+		self.add({
+			id:"credits_btn_k", type:"Button", x:382, y:470, size:"long"
+			text_id:"testing",
+			active:true
+		});
 	}
 });
